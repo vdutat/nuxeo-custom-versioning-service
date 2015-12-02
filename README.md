8b84bd98334736e9a2df5880fbdf0c652312906b
Vincent Dutat <vdutat@nuxeo.com>
2015-12-02 11:31:19 -0800

    first commit - Nx 6.0

---
+++ README.md
@@ -0,0 +1,35 @@
+# About feature
+
+Just before saving a document in the document repository, the `versioning service` calls a piece of code that could change the lifecycle state of the document. i.e. when a `File` document in state `approved` is modified, its state is put back to `project` by following the transition `backToProject`, this is the standard behaviour for a document having the lifecycle policy `default`.
+But when you are using custom document types having their own custom lifecyle policies, this behaviour makes no sense, as the transition `backToProject` may not exist.
+This behaviour can be changed by contributing a `Java` class to [extension point `versioningService` of service `VersionService`](http://explorer.nuxeo.com/nuxeo/site/distribution/Nuxeo%20Platform-6.0/viewExtensionPoint/org.nuxeo.ecm.core.versioning.VersioningService--versioningService)
+This module contributes such a `Java` class, and allows you to specify what transition to follow using some configuration variables in configuration file `nuxeo.conf`.
+i.e. a document of type `docType1` with a lifecycle policy `lcPolicy1` must follow transition `backToDraft` when it is modified and has the lifecycle state `validated`, here is the configuration parameter to put in `nuxeo.conf`:
+
+```
+nuxeo.versioning.versioningservice.lcPolicy1.from.validated.followTransition=backToDraft
+```
+
+If you don't define any configuration parameter in `nuxeo.conf`, the standard behaviour (following the transition `backToProject`) is disabled.
+
+This module is **not** supported by Nuxeo.
+
+* Project status: prototype
+ 
+# Building
+ 
+        mvn clean install
+  
+## QA
+  
+None.
+ 
+## Deploying
+
+2 options:
+
+- copy the JAR bundle in `$NUXEO_HOME/templates/custom/bundles/` and activate the `custom` template (see Nuxeo documentation [Configuration Templates](https://doc.nuxeo.com/display/ADMINDOC/Configuration+Templates)).
+- **OR** copy the JAR bundle in `$NUXEO_HOME/nxserver/plugins/` or `$NUXEO_HOME/nxserver/bundles/`
+
+Then restart the server.
+
---
+++ src/main/java/org/nuxeo/ecm/core/versioning/ConfigVersioningService.java
@@ -0,0 +1,47 @@
+package org.nuxeo.ecm.core.versioning;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.DocumentException;
+import org.nuxeo.ecm.core.api.VersioningOption;
+import org.nuxeo.ecm.core.lifecycle.LifeCycleException;
+import org.nuxeo.ecm.core.model.Document;
+import org.nuxeo.runtime.api.Framework;
+//import org.nuxeo.runtime.services.config.ConfigurationService;
+
+public class ConfigVersioningService extends StandardVersioningService implements VersioningService {
+	
+	public static final String AUTOMATION_CHAIN_PROP_PATTERN = "nuxeo.versioning.versioningservice.%s.from.%s.followTransition";
+
+    private static final Log LOGGER = LogFactory.getLog(ConfigVersioningService.class);
+
+    public static final String DEFAULT_LIFECYCLE_POLICY = "default";
+
+	@Override
+	protected void followTransitionByOption(Document doc, VersioningOption option) throws DocumentException {
+        try {
+            String transition = getTransition(doc.getLifeCyclePolicy(), doc.getLifeCycleState());
+            if (transition != null) {
+                LOGGER.debug("Following transition '" + transition + "'");
+                doc.followTransition(transition);
+            } else if (DEFAULT_LIFECYCLE_POLICY.equals(doc.getLifeCyclePolicy())) {
+                LOGGER.debug("Calling " + DEFAULT_LIFECYCLE_POLICY + " 'followTransitionByOption' implementation");
+                super.followTransitionByOption(doc, option);
+            } else {
+                LOGGER.debug("No transition followed");
+            }
+        } catch (LifeCycleException e) {
+            LOGGER.error(e, e);
+        }
+	}
+
+	protected String getTransition(String lifecyclePolicy, String lifecycleState) {
+        return Framework.getProperty(String.format(AUTOMATION_CHAIN_PROP_PATTERN, lifecyclePolicy, lifecycleState));
+	}
+
+//	protected String getTransition(String lifecyclePolicy, String lifecycleState) {
+//        ConfigurationService cs = Framework.getService(ConfigurationService.class);
+//        return cs.getProperty(String.format(AUTOMATION_CHAIN_PROP_PATTERN, lifecyclePolicy, lifecycleState));
+//	}
+
+}
